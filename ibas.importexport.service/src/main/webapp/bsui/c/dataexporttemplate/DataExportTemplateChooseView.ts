0e66eb65ec868364e2cf131d7e42716ea38f1048
牛加人等于朱 <niuren.zhu@icloud.com>
2017-11-02 17:23:58 +0800

    更改对话框机制

--- ibas.importexport.service/src/main/webapp/bsui/c/dataexporttemplate/DataExportTemplateChooseView.ts
+++ ibas.importexport.service/src/main/webapp/bsui/c/dataexporttemplate/DataExportTemplateChooseView.ts
@@ -78,7 +78,7 @@ export class DataExportTemplateChooseView extends ibas.BOChooseView implements I
                 that.fireViewEvents(that.fetchDataEvent, criteria);
             }
         });
-        return this.table;
+        return new sap.m.Dialog("", {            title: this.title,            type: sap.m.DialogType.Standard,            state: sap.ui.core.ValueState.None,            stretchOnPhone: true,            horizontalScrolling: true,            verticalScrolling: true,            content: [this.table],            buttons: [this.darwBars()]        });
     }
     private table: sap.ui.table.Table;
     /** 显示数据 */
