f1ed45749621a85fc95b5be4347eaa434815e70d
牛加人等于朱 <niuren.zhu@icloud.com>
2017-11-28 12:57:27 +0800

    模块常量移至api中

--- ibas.importexport.service/src/main/webapp/bsapp/Console.ts
+++ ibas.importexport.service/src/main/webapp/bsapp/Console.ts
@@ -7,6 +7,7 @@
  */
 
 import * as ibas from "ibas/index";
+import { CONSOLE_ID, CONSOLE_NAME, CONSOLE_VERSION } from "../api/index";
 import {
     DataExportTemplateFunc,
     DataExportTemplateChooseServiceMapping,
@@ -25,18 +26,12 @@ import {
 
 /** 模块控制台 */
 export class Console extends ibas.ModuleConsole {
-    /** 模块-标识 */
-    static CONSOLE_ID: string = "cbd4ef0f-ee88-4ea7-96c7-13f974026b58";
-    /** 模块-名称 */
-    static CONSOLE_NAME: string = "ImportExport";
-    /** 模块-版本 */
-    static CONSOLE_VERSION: string = "0.1.0";
     /** 构造函数 */
     constructor() {
         super();
-        this.id = Console.CONSOLE_ID;
-        this.name = Console.CONSOLE_NAME;
-        this.version = Console.CONSOLE_VERSION;
+        this.id = CONSOLE_ID;
+        this.name = CONSOLE_NAME;
+        this.version = CONSOLE_VERSION;
         this.copyright = ibas.i18n.prop("shell_license");
     }
     private _navigation: ibas.IViewNavigation;
