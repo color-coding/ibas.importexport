e15f87041b02c316ccf970e89ee91cfb67a1e6da
niurenzhu <niuren.zhu@icloud.com>
2017-12-03 12:36:27 +0800

    独立枚举资源文件

--- ibas.importexport.service/src/main/webapp/bsapp/Console.ts
+++ ibas.importexport.service/src/main/webapp/bsapp/Console.ts
@@ -60,6 +60,7 @@ export class Console extends ibas.ModuleConsole {
     run(): void {
         // 加载语言-框架默认
         ibas.i18n.load(this.rootUrl + "resources/languages/importexport.json");
+        ibas.i18n.load(this.rootUrl + "resources/languages/enums.json");
         ibas.i18n.load(this.rootUrl + "resources/languages/bo/dataexporttemplate.json");
         // 设置资源属性
         this.description = ibas.i18n.prop(this.name.toLowerCase());
