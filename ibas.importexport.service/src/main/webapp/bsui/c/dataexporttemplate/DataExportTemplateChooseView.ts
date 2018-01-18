fdcc629a1c1b2cfc127da544c9dc575ac299e4d5
牛加人等于朱 <niuren.zhu@icloud.com>
2018-01-18 12:06:26 +0800

    调整选择页面表格选择风格

--- ibas.importexport.service/src/main/webapp/bsui/c/dataexporttemplate/DataExportTemplateChooseView.ts
+++ ibas.importexport.service/src/main/webapp/bsui/c/dataexporttemplate/DataExportTemplateChooseView.ts
@@ -64,7 +64,8 @@ export class DataExportTemplateChooseView extends ibas.BOChooseView implements I
             columns: [
             ]
         });
-        this.id = this.table.getId();
+        // 调整选择样式风格
+        openui5.utils.changeSelectionStyle(this.table, this.chooseType);
         // 添加列表自动查询事件
         openui5.utils.triggerNextResults({
             listener: this.table,
