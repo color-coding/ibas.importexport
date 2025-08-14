e10bb457c82b80a3ac7746989071125df936f739
Niuren.Zhu <niuren.zhu@icloud.com>
2025-08-14 16:12:13 +0800

    优化代码

--- ibas.importexport.service/src/main/webapp/bsapp/Console.ts
+++ ibas.importexport.service/src/main/webapp/bsapp/Console.ts
@@ -49,9 +49,9 @@ namespace importexport {
             /** 运行 */
             run(): void {
                 // 加载语言-框架默认
-                ibas.i18n.load([
-                    this.rootUrl + "resources/languages/importexport.json",
-                    this.rootUrl + "resources/languages/bos.json"
+                this.loadResources([
+                    "resources/languages/importexport.json",
+                    "resources/languages/bos.json"
                 ], () => {
                     // 设置资源属性
                     this.description = ibas.i18n.prop(this.name.toLowerCase());
